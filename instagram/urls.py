b5eb49a85d071234b24b7d25e2a94d99fa55daa1
yvette <yvetteadan2000@gmail.com>
2020-01-07 11:01:28 +0300

    Add changes

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,9 +13,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+from django.contrib.auth import views as auth_views
+from .views import *
+
+urlpatterns=[   
+    url(r'^$', views.index, name='index'),
+    url(r'^uprofile/', views.uprofile, name='uprofile'),
+    url(r'^register/', signup, name='signup'),
+    url(r'^account/', include('django.contrib.auth.urls')),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name="registration/logout.html"), name='logout'),
+    url(r'^register/', signup, name='signup'),
+    url(r'^accounts/index',views.index,name="index"),
+    url(r'^accounts/profile',views.profile,name="profile"),
+    url(r'^profile',views.profile,name="profile"),
+    
+    # url(r'^profile/<username>/',views.user_profile, name='user_profile'),
+    url(r'^user_profile/<username>/', user_profile, name='user_profile'),
+    url(r'^post/<id>', post_comment, name='comment'),
+    url(r'^like', like_post, name='like_post'),
+    url(r'^search/', search_profile, name='search'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
