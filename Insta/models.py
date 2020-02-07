262593246371e922792699033d7927c3d6c92074
yvette <yvetteadan2000@gmail.com>
2020-02-07 12:43:43 +0300

    add class post

--- Insta/models.py
+++ Insta/models.py
@@ -4,47 +4,34 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
     profile_pic = models.ImageField(upload_to='images/', default='default.png')
     bio = models.TextField(max_length=500, default="Users biography", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
-
+    
 
      
     
 class Post(models.Model):
-    image = models.ImageField(upload_to='posts/')
-    name = models.CharField(max_length=250, blank=True)
-    caption = models.CharField(max_length=250, blank=True)
-    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
-
-    class Meta:
-        db_table = 'posts'
-        
-    @property
-    def get_all_comments(self):
-        return self.comments.all()
-
-    @property
-    def save_image(self):
-        self.save()
+    caption = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'posts/')
 
     @classmethod
-    def get_all_posts(cls):
-        return cls.objects.all()
+    def get_posts(cls):
+        posts = cls.objects.all()
+        return posts
 
-    @property
-    def delete(self):
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
         self.delete()
-
-    def total_likes(self):
-        return self.likes.count()
-
-    def __str__(self):
-        return f'{self.user.name} Post'
     
 class Comment(models.Model):
     comment = models.TextField()
